1d50ab4dfdc3dcbe2249c5aca99cd3e4509f86ea
li671207 <635245976@qq.com>
2020-04-06 13:58:00 +0800

    spring cloud alibaba-sentinel实现熔断与限流

---
+++ cloudalibaba-sentinel-server8401/src/main/java/com/lihl/springcloud/alibaba/SentinelMain8401.java
@@ -0,0 +1,17 @@
+package com.lihl.springcloud.alibaba;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SentinelMain8401 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentinelMain8401.class, args);
+	}
+
+}
---
+++ cloudalibaba-sentinel-server8401/src/main/java/com/lihl/springcloud/alibaba/controller/FlowLimitController.java
@@ -0,0 +1,27 @@
+package com.lihl.springcloud.alibaba.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+@RestController
+public class FlowLimitController {
+
+	@GetMapping("/testA")
+	public String testA() {
+		try {
+			TimeUnit.SECONDS.sleep(1);//测试流控线程数
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return "------testA";
+	}
+
+	@GetMapping("/testB")
+	public String testB() {
+		return "------testB";
+	}
+
+}
