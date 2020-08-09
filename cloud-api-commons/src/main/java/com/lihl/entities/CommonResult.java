6b0e7f2c947da0f30e57ea198767ad9eed308599
li671207 <635245976@qq.com>
2020-08-09 16:29:56 +0800

    test

--- cloud-api-commons/src/main/java/com/lihl/entities/CommonResult.java
+++ cloud-api-commons/src/main/java/com/lihl/entities/CommonResult.java
@@ -1,8 +1,15 @@
 package com.lihl.entities;
 
+import cn.hutool.Hutool;
+import cn.hutool.core.lang.Snowflake;
+import cn.hutool.core.net.NetUtil;
+import cn.hutool.core.util.IdUtil;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Data
 @AllArgsConstructor
@@ -15,6 +22,21 @@ public class CommonResult<T> {
 
 	public CommonResult(Integer code, String message){
 		this(code, message, null);
+	}
+
+	public static void main(String[] args) {
+//		long wordId = NetUtil.ipv4ToLong(NetUtil.getLocalhostStr());
+//		System.out.println(wordId);
+		Snowflake snowflake = IdUtil.createSnowflake(1,1);
+		String s = "1000101011111010100011101011100011001010000100001000000000000";
+		System.out.println(s.length());
+		Date date = new Date();
+		date.setTime(1251809354202615808l);
+		SimpleDateFormat ss = new SimpleDateFormat("yyyyMMdd");
+
+		System.out.println(ss.format(date));
+
+		System.out.println(snowflake.nextId());
 	}
 
 }
